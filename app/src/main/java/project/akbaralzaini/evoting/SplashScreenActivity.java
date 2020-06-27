1c22a35ea2a9551863c8ebeba8cd2f513effe0e6
akbaralzaini <akbaralzaini@gmail.com>
2020-06-27 12:04:50 +0700

    Initial commit

---
+++ app/src/main/java/project/akbaralzaini/evoting/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package project.akbaralzaini.evoting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    private int waktu_loading=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
